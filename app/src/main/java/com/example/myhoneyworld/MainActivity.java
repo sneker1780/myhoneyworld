0def3df4965c08206a4147f5c18208989744f4f1
Serhii <sneker1780@gmail.com>
2019-03-22 21:47:39 +0200

    dobavleno name and status razrabotchika

--- app/src/main/java/com/example/myhoneyworld/MainActivity.java
+++ app/src/main/java/com/example/myhoneyworld/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a=1;
-        int b=1;
+        String Serhii;
+        int razrabotchik=100;
     }
 }
